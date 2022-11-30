943b86ea3db284b786a91cda80c7e5331e610950
Afolabi <opakunleafolabi@gmail.com>
2022-11-30 16:08:22 +0100

    base set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
